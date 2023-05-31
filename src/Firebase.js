3a5c6982cc4e1a67081a1715de82ec67ea237e25
Pradip369 <kpuniverse369@gmail.com>
2023-05-31 21:52:41 +0530

    first commit

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,13 +1,13 @@
 import firebase, { firestore } from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCQHJHOf4b3sv6Et64MXnWBnc6o6fIUmpw",
-    authDomain: "clone-36-3c2b2.firebaseapp.com",
-    databaseURL: "https://clone-36-3c2b2.firebaseio.com",
-    projectId: "clone-36-3c2b2",
-    storageBucket: "clone-36-3c2b2.appspot.com",
-    messagingSenderId: "864181351533",
-    appId: "1:864181351533:web:685957b3811a6e04443b64"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const FirebaseApp = firebase.initializeApp(firebaseConfig);
